cd573b36f55aaed7b76bc100c813fb8e46b78a18
Volokonskiy Alex <volokonskiy52@gmail.com>
2019-07-29 12:15:23 +0300

    update TeamLead

--- teamLead.php
+++ teamLead.php
@@ -1,24 +1,29 @@
 <?php
 
-
 class teamLead
 {
     protected $moodLead;
-    protected $state = [['stateName' => 'he state "do not fall for eyes', 'pharse' => 'some phare'], 'Bad mood', 'Normal mood', 'Good mood'];
+    protected $state = [
+        ['stateName' => 'he state "do not fall for eyes', 'pharse' => 'some phare'],
+        'Bad mood',
+        'Normal mood',
+        'Good mood'
+    ];
 
     public function __construct()
     {
         $this->moodLead = mt_rand(0, 3);
     }
-    public function getMoodTeamLead()
-    {
-        return $this->moodLead;
-    }
 
     public function getLeadState()
     {
         $stateId = $this->getMoodLead();
-
         return $this->state[$stateId];
+    }
+
+    public function getMoodLead()
+    {
+        return ($this->lead + $this->getWorkJun() < count($this->state)) ? $this->lead +
+            $this->getWorkJun() : count($this->state) - 1;
     }
 }
